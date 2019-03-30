d32829fc7106a8e4f4cb81133ecbd06dc3303334
hqa11 <hqa4653297>
2019-03-31 03:26:55 +0800

    first11

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -16,7 +16,6 @@ public class IndexController {
     public String hello(String name){
         // 获取IP地址
         try {
-            1
             String ip = InetAddress.getLocalHost().getHostAddress();
             String msg = String.format("Hello!you are %s,the ip is %s !",name,ip);
             return msg;
